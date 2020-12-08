399417614ce9a8579a7cc914a8b4da3d2fa09b85
eduard moreno <katananata500@gmail.com>
2020-12-08 23:52:38 +0000

    fix error about if elif

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,11 +3,11 @@ import random
 number = random.randint(-10000, 10000)
 if number > 0:
     last_digit = number % 10
-if number < 0:
+elif number < 0:
     last_digit = number % -10
 if last_digit == 0:
     print("Last digit of {} is 0".format(number), "and is 0")
 elif last_digit < 6:
     print("Last digit of {} is {}".format(number, last_digit), "and is and is less than 6")
-elif last_digit > 5:
+else:
     print("Last digit of {} is {}".format(number, last_digit), "and is greater than 5")

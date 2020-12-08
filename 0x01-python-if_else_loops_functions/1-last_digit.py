fa6aeb88aa5e7769e9fc5fdf716c8f51f510e756
eduard moreno <katananata500@gmail.com>
2020-12-08 23:43:28 +0000

    fix and adding <0 option

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
-print("Last digit of the number is", last_digit) 
-if last_digit > 5:
-    print("and is greater than 5")
-elif last_digit == 0:
-    print("and is 0")
+if number > 0:
+    last_digit = number % 10
+if number < 0:
+    last_digit = number % -10
+if last_digit == 0:
+    print("Last digit of {} is 0".format(number), "and is 0")
 elif last_digit < 6:
-    print("and is less than 6 and not 0")
+    print("Last digit of {} is {}".format(number, last_digit), "and is and is less than 6")
+elif last_digit > 5:
+    print("Last digit of {} is {}".format(number, last_digit), "and is greater than 5")

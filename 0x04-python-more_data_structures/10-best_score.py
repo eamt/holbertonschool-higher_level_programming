44a08e02bce2a5f8a53ab497b9537296ffbe0a0c
eduard moreno <katananata500@gmail.com>
2020-12-18 03:53:54 +0000

    fix 10 typo

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,6 +4,6 @@ def best_score(my_dict):
         return (None)
     if isinstance(my_dict, dict) is False
         return (None)
-    if max(my_dict, key=mydict.get) is 0:
+    if max(my_dict, key=my_dict.get) is 0:
         return (None)
     return(max(my_dict, key=my_dict.get))

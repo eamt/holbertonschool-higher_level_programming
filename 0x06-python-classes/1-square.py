2161a21a2cdfa0934a4e9fc3a7f1326e652c578b
eduard moreno <katananata500@gmail.com>
2021-01-06 00:18:50 +0000

    1 square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """
+    class square that has attributes:
+        size
+    """
+    def __init__(self, size):
+        """
+        the initialization of the method for the square class
+        """
+        self.__size = size

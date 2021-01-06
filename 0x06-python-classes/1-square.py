bd66f2f45b1cb69864f3c793ecb5272794b16064
eduard moreno <katananata500@gmail.com>
2021-01-06 00:23:59 +0000

    1-square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """This is my first project with  documentation in Python """
 
+
+
 class Square:
-    """
-    class square that has attributes:
+    """ class square that has attributes:
         size
     """
+
     def __init__(self, size):
         """
         the initialization of the method for the square class

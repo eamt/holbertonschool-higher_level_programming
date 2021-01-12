9badb394c3651ab6e9b8191356621086cc7aa6a6
eduard moreno <katananata500@gmail.com>
2021-01-12 00:54:46 +0000

    6-rectangle

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Module to create a rectangle'''
 
+
 class Rectangle:
     '''Rectangle class'''
     number_of_instances = 0;
@@ -15,29 +16,29 @@ class Rectangle:
 
     @property
     def width(self):
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
         '''setter width'''
         if type(value) is not int:
-            raise typeError("width is not integer")
+            raise TypeError("width is not integer")
         if value < 0:
-            raise TypeError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
         '''getter for heihgt property'''
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
         '''setter height'''
         if type(value) is not int:
-            raise TypeError("height is not an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise TypeError("height must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):

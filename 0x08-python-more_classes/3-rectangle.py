23d99019765b4adb608b3e3091511a6cef69cbf7
eduard moreno <katananata500@gmail.com>
2021-01-12 00:04:55 +0000

    fixing typeError fo TypeError

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
     def width(self, value):
         '''setter width'''
         if type(value) is not int:
-            raise typeError("width must be an integer")
+            raise TypeError("width must be an integer")
         if value < 0:
             raise TypeError("width must be >= 0")
         self.__width = value

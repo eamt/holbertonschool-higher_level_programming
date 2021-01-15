1bae80d4f771d35ac5bd0d538dec4bb00b85dda6
eduard moreno <katananata500@gmail.com>
2021-01-15 23:38:22 +0000

    3 say may name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Say My Name - a function that says your name.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Says your name.
+    first_name and last_name must be strings otherwise, will raise a TypeError
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {:s} {:s}".format(first_name, last_name))

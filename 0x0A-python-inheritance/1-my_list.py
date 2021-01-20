af29e54610dcc37c92c5e2d9d6c6ef691e23f185
eduard moreno <katananata500@gmail.com>
2021-01-20 00:08:22 +0000

    my list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"Module to define a class named MyList"
+
+
+class MyList(list):
+    """A class named MyList"""
+
+    def print_sorted(self):
+        """Prints instance"""
+        print(sorted(self))

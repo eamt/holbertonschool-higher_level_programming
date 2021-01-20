b437d459ec96c016f39771435028c2ac833e6220
eduard moreno <katananata500@gmail.com>
2021-01-20 01:18:59 +0000

    rectangle fix

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module to create a Rectangle class"""
+"""Module to  create a Rectangle class from BaseGeometry"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
@@ -16,9 +16,16 @@ class Rectangle(BaseGeometry):
             attr1(width): width of rectangle
             attr2(height): height of rectangle
         """
-    
+
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-
         self.__width = width
         self.__height = height
+
+    def area(self):
+        """Return area of the instance"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return a string representation"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)

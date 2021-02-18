3f9077f138ba70419722e43d0197c98d8a64b8fb
eduard moreno <katananata500@gmail.com>
2021-02-18 01:19:13 +0000

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,3 +14,14 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Init builder"""
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        """size setter"""
+        self.width = size
+        self.height = size

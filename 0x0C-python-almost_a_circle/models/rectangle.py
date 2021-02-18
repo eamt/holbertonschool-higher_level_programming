f3abfb4b51f96e3c57b1fc72cbcdce463eb43efd
eduard moreno <katananata500@gmail.com>
2021-02-18 00:38:29 +0000

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,3 +24,116 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+    def __str__(self):
+        """Overloads the __str__ method to print specific syntax"""
+
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height)
+
+    @property
+    def width(self):
+        """Getter of the width attribute
+        Returns:
+            the width of the instance
+        """
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for the width attribute
+        Args:
+            value (int): value to set to width
+        """
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for the height attribute
+        Returns:
+            the height of the instance
+        """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for the height attribute
+        Args:
+            value (int): the value to set the height to
+        """
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for the x attribute
+        Returns:
+            the x attribute of the instance
+        """
+
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter for the x attribute
+        Args:
+            value (int): the value to set the x attribute to
+        """
+
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for the y attribute
+        Returns:
+            the y attribute of the instance
+        """
+
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter of the y attribute
+        Args:
+            value (int): the value to set the y attribute to
+        """
+
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Area of the rectangle instance
+        Returns:
+            the area of the current rectangle instance
+        """
+
+        return self.__width * self.__height
+
+    def display(self):
+        """Displays the rectangle with a # sign
+        """
+        for y in range(self.__y):
+            print()
+        for i in range(self.__height):
+            for x in range(self.__x):
+                print(' ', end="")
+            for j in range(self.__width):
+                print('#', end="")
+            print()

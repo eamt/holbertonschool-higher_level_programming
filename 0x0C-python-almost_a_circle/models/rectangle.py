4de7c45ab6ef49ea98e77a02e16a4800efd48d50
eduard moreno <katananata500@gmail.com>
2021-02-18 00:51:58 +0000

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -137,3 +137,36 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print('#', end="")
             print()
+    def update(self, *args, **kwargs):
+        """Updates the attributes in a list *args or **kwargs
+        Args:
+            *args (list): non-keyworded argument list
+            **kwargs (dict): keyworded argument list
+        """
+
+        if len(args) > 0:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[0]
+                if i == 1:
+                    self.width = args[1]
+                if i == 2:
+                    self.height = args[2]
+                if i == 3:
+                    self.x = args[3]
+                if i == 4:
+                    self.y = args[4]
+        else:
+            if len(kwargs) > 0:
+                keys = kwargs.keys()
+                for i in keys:
+                    if i == 'id':
+                        self.id = kwargs['id']
+                    if i == 'width':
+                        self.width = kwargs['width']
+                    if i == 'height':
+                        self.height = kwargs['height']
+                    if i == 'x':
+                        self.x = kwargs['x']
+                    if i == 'y':
+                        self.y = kwargs['y']

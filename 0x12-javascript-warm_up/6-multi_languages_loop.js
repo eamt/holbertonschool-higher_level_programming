a6e01f703d026b35ea65b5a93b11b5d962196b7b
eduard moreno <katananata500@gmail.com>
2021-03-22 22:34:27 +0000

    fix 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const lenguajes = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const lenguajes = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < lenguajes.length; i++) {
   console.log(lenguajes[i]);

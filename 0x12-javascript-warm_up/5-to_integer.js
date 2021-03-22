159aa0b96804905b5224e7a728e221f66828d6a7
eduard moreno <katananata500@gmail.com>
2021-03-22 22:31:54 +0000

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const argumentos = parseInt(process.argv[2]);
 
 if (argumentos) {
-  console.log('My number:' + argumentos);
+  console.log('My number: ' + argumentos);
 } else {
   console.log('Not a number');
 }

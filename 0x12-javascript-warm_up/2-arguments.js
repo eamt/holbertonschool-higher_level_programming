3bdcaf374085e309e1fc83ee79bad12a9740549e
eduard moreno <katananata500@gmail.com>
2021-03-22 21:34:55 +0000

    fix 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,4 +4,6 @@ if (argumentos === 3) {
   console.log('Argument found');
 } if (argumentos === 2) {
   console.log('No argument');
-} else console.log('Argument found');
+} else {
+  console.log('Argument found');
+}

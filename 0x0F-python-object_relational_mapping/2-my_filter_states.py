8c31fb2112fa835edeabe0fd9be908501690f9dc
eduard moreno <katananata500@gmail.com>
2021-03-28 01:14:14 +0000

    2 fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
 """
 Script that lists all states from the
-database hbtn_0e_0_usa.
+database hbtn_0e_0_usa. with a name 
 """
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    """
-    Access to the database and get states
-    """
-    db = MySQLdb.connect(host="localhost", port=3306, 
-                         user=argv[1], passwd=argv[2], 
-                         db=argv[3], charset="utf8")
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY \
     id ASC".format(argv[4]))
     rows = cursor.fetchall()
     for row in rows:

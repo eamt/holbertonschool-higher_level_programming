a12773d960bc36bc5770563188a7e0812fc96d6f
eduard moreno <katananata500@gmail.com>
2021-03-29 03:48:49 +0000

    states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Script that lists all states from the
-database hbtn_0e_0_usa. with a name 
-"""
+"""Script that lists all states from the database hbtn_0e_0_usa."""
 
 import MySQLdb
 from sys import argv
